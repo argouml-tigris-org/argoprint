47857732620f3c1911491279a0428834263771fb
Mattias Danielsson <matda701@tigris.org>
2003-11-16 20:45:13 +0000

    initial setup
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@11 39b8678e-d811-0410-9f66-c214488f071d

---
+++ src/org/argoprint/engine/interpreters/Interpreter.java
@@ -0,0 +1,27 @@
+package org.argoprint.engine.interpreters;
+
+//import org.apache.xerces.*;
+
+import org.apache.xerces.parsers.DOMParser;
+
+//import org.w3c.*;
+
+import org.argoprint.engine.*;
+
+
+/**
+ * Superclass for the interpreters.
+ */
+abstract class Interpreter{
+    private String tagName;
+    
+//      public Node handleTag(Node tagNode, Environment env){
+//  	return null;
+//      }
+
+//      abstract private Node processTag(Node tagNode, Environment env);
+
+//      private boolean canHandle(Node tagNode){
+//  	return false;
+//      } 
+}
