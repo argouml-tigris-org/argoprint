c6987f2f9dd02286ffa5adf48678d63d8b0e628a
Anders Olsson <anderso@tigris.org>
2003-11-17 01:11:12 +0000

    Added writing of the output. For now it just copies the input.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@17 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/Main.java
+++ src/org/argoprint/engine/Main.java
@@ -1,16 +1,29 @@
 package org.argoprint.engine;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 import org.apache.xerces.parsers.DOMParser;
+import org.apache.xml.serialize.*;
 
 class Main {
-	private DOMParser _parser;
+	
 	public Main() {
 	}
-	public void processTemplate(String template, String outputFile, String outputDir) {
-		try {
-		_parser.parse(template);
-		}
-		catch (Exception e) {
-			System.out.println("Error in parsing template: " + e.getMessage());
-		}
+	
+	public void processTemplate(String template, String outputFile, String outputDir) 
+	throws SAXException, IOException, FileNotFoundException {
+		DOMParser parser = new DOMParser(); 
+		parser.parse(template);
+		Document doc = parser.getDocument();
+		
+		// TODO - initialize the CoR and send it the document
+		
+		FileOutputStream outputStream = new FileOutputStream(outputFile);
+		XMLSerializer serializer = new XMLSerializer(outputStream, null);
+		// Currently only outputs the input document
+		serializer.serialize(doc);
 	}
 }
