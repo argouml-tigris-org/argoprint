6bed794afd2b58b88db60b81c8f89602c62a54f3
Anders Olsson <anderso@tigris.org>
2003-11-18 00:59:24 +0000

    Implemented all methods and added some instance variables
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@26 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/interpreters/Interpreter.java
+++ src/org/argoprint/engine/interpreters/Interpreter.java
@@ -1,27 +1,70 @@
 package org.argoprint.engine.interpreters;
 
-//import org.apache.xerces.*;
-
-import org.apache.xerces.parsers.DOMParser;
-
 import org.w3c.dom.Node;
-
 import org.argoprint.engine.*;
-
+import org.argoprint.uml_interface.UMLInterface;
 
 /**
  * Superclass for the interpreters.
  */
 abstract class Interpreter{
-    private String tagName;
+    private String _tagName;
+    private String _prefix;
+    private Interpreter _nextHandler;
+    // TODO this should be changed when the interface is defined later
+	// processTag uses calls to Interface
+    private UMLInterface _umlInterface;
+    // Interpreter needs to use interpret() and getDocument() in the Main object
+    private Main _main;
     
-    public Node handleTag(Node tagNode, Environment env){
-	return null;
+    /**
+	 * @param tagName The name of the tag that this Interpreter can process.
+	 * @param prefix The namespace prefix of the tag that this Interpreter can process.
+	 * @param umlInterface The interface that this Interpreter should make calls to.
+	 * @param main The Main object. 
+	 */
+	public Interpreter(String tagName, String prefix, UMLInterface umlInterface, Main main) {
+    	_tagName = tagName;
+    	_prefix = prefix;
+    	_umlInterface = umlInterface;
+    	_main = main;
+    }
+    
+    /**
+     * Specifies the next Interpreter in the chain of responsibility.
+     * 
+	 * @param nextHandler The next Interpreter.
+	 */
+	public void setNextHandler(Interpreter nextHandler) {
+    	_nextHandler = nextHandler;
+    }
+    
+    /**
+     * Processes the Node or sends it to the next Interpreter.
+     *  
+	 * @param tagNode The Node to handle.
+	 * @param env The environment in which the Node is to be processed.
+	 * @return 
+	 */
+	public Node handleTag(Node tagNode, Environment env){
+		if (this.canHandle(tagNode))
+			return this.processTag(tagNode, env);
+		else
+			return _nextHandler.handleTag(tagNode, env);		
     }
 
     protected abstract Node processTag(Node tagNode, Environment env);
 
-    private boolean canHandle(Node tagNode){
-	return false;
+    /**
+     * Checks if this Interpreter can handle this Node.
+     * 
+	 * @param tagNode
+	 * @return
+	 */
+	private boolean canHandle(Node tagNode){
+    	if (tagNode.getNodeType() == Node.ELEMENT_NODE && tagNode.getLocalName() == _tagName && tagNode.getPrefix() == _prefix)
+			return true;
+		else
+			return false;
     } 
 }
