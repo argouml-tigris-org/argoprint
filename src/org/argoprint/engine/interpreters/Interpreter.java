a1fd1211ecf7f26bd482c22fe4e42179972b1fc1
Anders Olsson <anderso@tigris.org>
2003-11-18 15:13:20 +0000

    Small changes
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@32 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/interpreters/Interpreter.java
+++ src/org/argoprint/engine/interpreters/Interpreter.java
@@ -7,15 +7,18 @@ import org.argoprint.uml_interface.UMLInterface;
 /**
  * Superclass for the interpreters.
  */
-abstract class Interpreter{
+abstract public class Interpreter{
     private String _tagName;
-    private String _prefix;
     private Interpreter _nextHandler;
     // TODO this should be changed when the interface is defined later
 	// processTag uses calls to Interface
     private UMLInterface _umlInterface;
-    // Interpreter needs to use interpret() and getDocument() in the Main object
-    private Main _main;
+    // Interpreter needs to use interpret() in the Main object
+    protected Main _main;
+    
+    public Interpreter() {
+    	_nextHandler = null;
+    }
     
     /**
 	 * @param tagName The name of the tag that this Interpreter can process.
@@ -23,9 +26,8 @@ abstract class Interpreter{
 	 * @param umlInterface The interface that this Interpreter should make calls to.
 	 * @param main The Main object. 
 	 */
-	public Interpreter(String tagName, String prefix, UMLInterface umlInterface, Main main) {
+	public Interpreter(String tagName, UMLInterface umlInterface, Main main) {
     	_tagName = tagName;
-    	_prefix = prefix;
     	_umlInterface = umlInterface;
     	_main = main;
     }
@@ -46,14 +48,18 @@ abstract class Interpreter{
 	 * @param env The environment in which the Node is to be processed.
 	 * @return 
 	 */
-	public Node handleTag(Node tagNode, Environment env){
-		if (this.canHandle(tagNode))
-			return this.processTag(tagNode, env);
+	public void handleTag(Node tagNode, Environment env)
+	throws Exception {
+		if (canHandle(tagNode))
+			processTag(tagNode, env);
 		else
-			return _nextHandler.handleTag(tagNode, env);		
+			if (_nextHandler == null)
+				throw new Exception("The last Interpreter in the chain could not handle the Node.");
+			else
+				_nextHandler.handleTag(tagNode, env);	
     }
 
-    protected abstract Node processTag(Node tagNode, Environment env);
+    protected abstract void processTag(Node tagNode, Environment env) throws Exception;
 
     /**
      * Checks if this Interpreter can handle this Node.
@@ -61,8 +67,8 @@ abstract class Interpreter{
 	 * @param tagNode
 	 * @return
 	 */
-	private boolean canHandle(Node tagNode){
-    	if (tagNode.getNodeType() == Node.ELEMENT_NODE && tagNode.getLocalName() == _tagName && tagNode.getPrefix() == _prefix)
+	protected boolean canHandle(Node tagNode){
+    	if (tagNode.getNodeType() == Node.ELEMENT_NODE && tagNode.getLocalName() == _tagName && tagNode.getPrefix() == "argoprint")
 			return true;
 		else
 			return false;
