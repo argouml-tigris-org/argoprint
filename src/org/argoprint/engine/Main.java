e1f3de6d3aad706fd44203229349d1a256eb027e
Anders Olsson <anderso@tigris.org>
2003-11-18 15:12:09 +0000

    Replaced processTemplate with initializeSystem and go as in the design. Added interpret.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@31 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/Main.java
+++ src/org/argoprint/engine/Main.java
@@ -1,29 +1,52 @@
 package org.argoprint.engine;
+import org.argoprint.engine.interpreters.*;
+import org.argoprint.uml_interface.UMLInterface;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+// import org.argoprint.ui.Settings;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 import org.apache.xerces.parsers.DOMParser;
 import org.apache.xml.serialize.*;
 
-class Main {
-	
+public class Main {
+	private Interpreter _firstHandler;
+	private DOMParser _parser;
+	// TODO remove 
+	private String _outputFile;
+	private UMLInterface _umlInterface;
+
 	public Main() {
+		_parser = new DOMParser(); 
+		_umlInterface = new UMLInterface();
+		// Initialize the CoR and send it the document
+		_firstHandler = new InterpreterDefault("", _umlInterface, this);
 	}
-	
-	public void processTemplate(String template, String outputFile, String outputDir) 
-	throws SAXException, IOException, FileNotFoundException {
-		DOMParser parser = new DOMParser(); 
-		parser.parse(template);
-		Document doc = parser.getDocument();
+
+	// TODO change parameter to Settings when that class is finished
+	public void initializeSystem(String template, String outputFile, String outputDir) 
+	throws SAXException, IOException {
+		_outputFile = outputFile;
+		// TODO set outputDir of interface
+		_parser.parse(template);
+	}
+
+	public void go()
+	throws FileNotFoundException, IOException, Exception {
+		Document document = _parser.getDocument();
+		interpret(document, new Environment());
 		
-		// TODO - initialize the CoR and send it the document
-		
-		FileOutputStream outputStream = new FileOutputStream(outputFile);
+		FileOutputStream outputStream = new FileOutputStream(_outputFile);
 		XMLSerializer serializer = new XMLSerializer(outputStream, null);
 		// Currently only outputs the input document
-		serializer.serialize(doc);
+		serializer.serialize(document);
+	}
+
+	public void interpret(Node node, Environment env) 
+	throws Exception {
+		_firstHandler.handleTag(node, env);
 	}
 }
