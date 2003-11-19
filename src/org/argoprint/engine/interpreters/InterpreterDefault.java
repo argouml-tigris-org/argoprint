57b8b263083e11ed7c57498d8520f3ab151a06d8
Anders Olsson <anderso@tigris.org>
2003-11-19 15:53:35 +0000

    Minor updates.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@45 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/interpreters/InterpreterDefault.java
+++ src/org/argoprint/engine/interpreters/InterpreterDefault.java
@@ -1,14 +1,13 @@
 package org.argoprint.engine.interpreters;
+import org.argoprint.ArgoPrintDataSource;
 import org.argoprint.engine.Environment;
-// TODO update this later
-import org.argoprint.uml_interface.UMLInterface;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class InterpreterDefault extends Interpreter {
 
-	public InterpreterDefault(UMLInterface umlInterface) {
-		super("", umlInterface);
+	public InterpreterDefault(ArgoPrintDataSource dataSource) {
+		super("", dataSource);
 	}
 
 	/**
@@ -23,7 +22,10 @@ public class InterpreterDefault extends Interpreter {
 			// recurse on all children except text nodes
 			for (int i = 0; i < children.getLength(); i++){
 				if (children.item(i).getNodeType() != Node.TEXT_NODE)
-					_firstHandler.handleTag(children.item(i), env);
+					if (_firstHandler == null)
+						throw new Exception("First handler is not specified in this Interpreter.");
+					else
+						_firstHandler.handleTag(children.item(i), env);
 			}
 		}
 	}
