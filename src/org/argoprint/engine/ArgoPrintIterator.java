a46c6eabb72970721c98e4589cdb60d152b4fdd1
Mattias Danielsson <matda701@tigris.org>
2003-11-19 01:47:18 +0000

    Initial version
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@36 39b8678e-d811-0410-9f66-c214488f071d

---
+++ src/org/argoprint/engine/ArgoPrintIterator.java
@@ -0,0 +1,43 @@
+package org.argoprint.engine;
+
+import java.util.*;
+
+/**
+ * Class which extends iterator too better keep track of current object
+ */
+public class ArgoPrintIterator implements Iterator{
+    private Iterator _iterator;
+
+    private Object _currentObject;
+
+    public ArgoPrintIterator(Iterator iter){
+	_iterator = iter;
+    }
+
+    public boolean hasNext(){ return _iterator.hasNext(); }
+    
+    public Object next(){
+	_currentObject = _iterator.next();
+	return _iterator;
+    }
+
+    public void remove() 
+	throws UnsupportedOperationException,
+	       IllegalStateException {
+	try{
+	    _iterator.remove();
+	} catch(UnsupportedOperationException e){
+	    throw e;
+	}
+	catch(IllegalStateException e){
+	    throw e;
+	}
+    }
+
+    
+    public Object currentObject(){
+	return _currentObject;
+    }
+
+
+}
