979c922b8a68ec37acece8cb7ea21d5b5812009d
Mattias Danielsson <matda701@tigris.org>
2003-11-20 16:20:45 +0000

    Changed back to real the real UMLInterface
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@62 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/Main.java
+++ src/org/argoprint/engine/Main.java
@@ -1,6 +1,12 @@
 package org.argoprint.engine;
 import org.argoprint.ArgoPrintDataSource;
 import org.argoprint.engine.interpreters.*;
+
+import org.apache.log4j.Logger;
+
+import org.argoprint.ui.Settings;
+import org.argoprint.uml_interface.*;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,43 +19,64 @@ import org.apache.xml.serialize.*;
 import org.argoprint.DataSourceStub;
 
 public class Main {
-	private Interpreter _firstHandler;
-	private DOMParser _parser;
-	// TODO: remove 
-	private String _outputFile;
-	private ArgoPrintDataSource _dataSource;
+    private Interpreter _firstHandler;
+    private DOMParser _parser;
+    // TODO: remove 
+    private String _outputFile;
+    private ArgoPrintDataSource _dataSource;
+    private Logger _log;
+    
+    public Main() {
+	_parser = new DOMParser(); 
+	_dataSource = new UMLInterface(); //new DataSourceStub();
 
-	public Main() {
-		_parser = new DOMParser(); 
-		_dataSource = new DataSourceStub();
-		// Initialize the CoR
-		Interpreter interpreterDefault = new InterpreterDefault(_dataSource);
-		Interpreter interpreterCall = new InterpreterCall(_dataSource);
-		Interpreter interpreterIterate = new InterpreterIterate(_dataSource);
-		_firstHandler = interpreterCall;
-		interpreterCall.setNextHandler(interpreterIterate);
-		interpreterCall.setFirstHandler(interpreterCall);
-		interpreterIterate.setNextHandler(interpreterDefault);
-		interpreterIterate.setFirstHandler(interpreterCall);
-		interpreterDefault.setFirstHandler(interpreterCall);
-	}
+	
+	
+	// Initialize the CoR
+	Interpreter interpreterDefault = new InterpreterDefault(_dataSource);
+	Interpreter interpreterCall = new InterpreterCall(_dataSource);
+	Interpreter interpreterIterate = new InterpreterIterate(_dataSource);
+	_firstHandler = interpreterCall;
+	interpreterCall.setNextHandler(interpreterIterate);
+	interpreterCall.setFirstHandler(interpreterCall);
+	interpreterIterate.setNextHandler(interpreterDefault);
+	interpreterIterate.setFirstHandler(interpreterCall);
+	interpreterDefault.setFirstHandler(interpreterCall);
+    }
 
-	// TODO: change parameter to Settings when that class is finished
-	public void initializeSystem(String template, String outputFile, String outputDir) 
+    // TODO: change parameter to Settings when that class is finished
+    public void initializeSystem(String template, 
+				 String outputFile, 
+				 String outputDir) 
 	throws SAXException, IOException {
-		_outputFile = outputFile;
-		// TODO: set outputDir of interface
-		_parser.parse(template);
-	}
+	
+	_outputFile = outputFile;
+	// TODO: set outputDir of interface
+	_parser.parse(template);
+    }
+    
+    public void initializeSystem(Settings settings, Logger log) 
+	throws SAXException, IOException{
 
-	public void go()
+	((UMLInterface)_dataSource).initialize(log);
+	_log = log;
+	_outputFile = settings.getOutputFile();
+	// TODO: set outputDir of interface
+	// ((UMLInterface)_dataSource).setOutputPath(settings.getOutputDir);
+	_parser.parse(settings.getTemplate());
+    }
+
+    
+    public void go()
 	throws FileNotFoundException, IOException, Exception {
-		Document document = _parser.getDocument();
-		_firstHandler.handleTag(document, new Environment());
+	Document document = _parser.getDocument();
+	_firstHandler.handleTag(document, new Environment());
 		
-		FileOutputStream outputStream = new FileOutputStream(_outputFile);
-		XMLSerializer serializer = new XMLSerializer(outputStream, null);
-		// Currently only outputs the input document
-		serializer.serialize(document);
-	}
+	FileOutputStream outputStream = new FileOutputStream(_outputFile);
+	XMLSerializer serializer = new XMLSerializer(outputStream, null);
+	// Currently only outputs the input document
+	serializer.serialize(document);
+    }
 }
+
+ 
