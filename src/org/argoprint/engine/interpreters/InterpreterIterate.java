79253fbeff25bc4454c1d2d0d99a98a94810a1db
Anders Olsson <anderso@tigris.org>
2003-11-23 17:12:21 +0000

    Added sorting.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@76 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/engine/interpreters/InterpreterIterate.java
+++ src/org/argoprint/engine/interpreters/InterpreterIterate.java
@@ -1,13 +1,13 @@
 package org.argoprint.engine.interpreters;
 
 import java.util.Collection;
+import java.util.TreeMap;
+import java.util.Iterator;
+
 import org.argoprint.ArgoPrintDataSource;
 import org.argoprint.engine.ArgoPrintIterator;
 import org.argoprint.engine.Environment;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 public class InterpreterIterate extends Interpreter {
 
@@ -17,9 +17,6 @@ public class InterpreterIterate extends Interpreter {
 
 	/**
 	 * Processes the iterate tag.
-	 * 
-	 * @param tagNode
-	 * @param env
 	 */
 	protected void processTag(Node tagNode, Environment env) throws Exception {
 		NamedNodeMap attributes = tagNode.getAttributes();
@@ -29,15 +26,20 @@ public class InterpreterIterate extends Interpreter {
 		if (!(callReturnValue instanceof Collection))
 			throw new Exception("The object returned from the call to the data source is not a collection.");
 		Collection iterateCollection = (Collection) callReturnValue;
-			
-		Node iteratorNameAttribute = attributes.getNamedItem("iteratorname");
-		if (iteratorNameAttribute == null) 
-			throw new BadTemplateException("Iterate tag contains no iteratorname attribute.");
-		String iteratorName = iteratorNameAttribute.getNodeValue();
 
-		// TODO: Implement sorting, for now I just ignore sortvalue
+		Node sortvalueAttribute = attributes.getNamedItem("sortvalue");
+		if (sortvalueAttribute != null) {
+			String sortvalue = sortvalueAttribute.getNodeValue();
+			iterateCollection = sortCollection(iterateCollection, sortvalue);
+		}
+			
+		Node iteratornameAttribute = attributes.getNamedItem("iteratorname");
+		if (iteratornameAttribute == null) 
+			throw new BadTemplateException("Iterate tag contains no iteratorname attribute.");
+		String iteratorname = iteratornameAttribute.getNodeValue();
+		
 		ArgoPrintIterator iterator = new ArgoPrintIterator(iterateCollection.iterator());
-		env.addIterator(iteratorName, iterator);
+		env.addIterator(iteratorname, iterator);
 
 		Document document = tagNode.getOwnerDocument();
 		Node parentNode = tagNode.getParentNode();
@@ -53,7 +55,30 @@ public class InterpreterIterate extends Interpreter {
 				_firstHandler.handleTag(newNode, env);
 			}
 		}
-		env.removeIterator(iteratorName);
+		env.removeIterator(iteratorname);
 		parentNode.removeChild(tagNode);
 	}
-}	
+	
+	/**
+	 * Sorts a Collection according to a String returned by a call to the data source for each object in the collection. 
+	 * 
+	 * @param collection The collection to sort.
+	 * @param sortvalue The call that will be applied to each object providing a value to sort on.
+	 * @return A sorted Collection.
+	 * @throws Exception
+	 */
+	private Collection sortCollection(Collection collection, String sortvalue) throws Exception {
+		Iterator iterator = collection.iterator();
+		TreeMap sortedMap = new TreeMap();
+		Object object;
+		Object returned;
+		while (iterator.hasNext()) {
+			object = iterator.next();
+			returned = _dataSource.caller(sortvalue, object);
+			if (!(returned instanceof String))
+				throw new Exception("The sortvalue function did not return a String.");
+			sortedMap.put(returned, object);
+		}
+		return sortedMap.values();
+	}
+}
