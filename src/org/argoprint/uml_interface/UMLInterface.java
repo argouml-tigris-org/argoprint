14df1bad86af45ac266a2d55074b8b121bf6a2a8
Mattias Danielsson <matda701@tigris.org>
2003-11-25 20:04:25 +0000

    added copyright disclaimer
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@91 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/uml_interface/UMLInterface.java
+++ src/org/argoprint/uml_interface/UMLInterface.java
@@ -1,3 +1,35 @@
+//$Id$
+//Copyright (c) 2003, Mikael Albertsson, Mattias Danielsson, Per Engström, 
+//Fredrik Gröndahl, Martin Gyllensten, Anna Kent, Anders Olsson, 
+//Mattias Sidebäck.
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without 
+//modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, 
+//  this list of conditions and the following disclaimer.
+// 
+//* Redistributions in binary form must reproduce the above copyright 
+//  notice, this list of conditions and the following disclaimer in the 
+//  documentation and/or other materials provided with the distribution.
+//
+//* Neither the name of the University of Linköping nor the names of its 
+//  contributors may be used to endorse or promote products derived from 
+//  this software without specific prior written permission. 
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+//ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+//LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+//SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+//CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+//THE POSSIBILITY OF SUCH DAMAGE.
+
 package org.argoprint.uml_interface;
 
 import org.argoprint.*;
@@ -53,7 +85,7 @@ import org.tigris.gef.persistence.*;
 /** 
  * The class ArgoPrint uses to interface to the ArgoUML model.
  * It primarily communicates with ModelFacade using java.lang.reflect.*
- * To save diagrams gef is called. //TODO: fix bug that causes only
+ * To save diagrams gef is used. TODO: fix bug that causes only
  * the open diagram to be saved correctly.
  *
  * @author matda701, Mattias Danielsson
@@ -161,7 +193,8 @@ public class UMLInterface
      */
     public Object caller(String call, Object iteratorObject)
 	throws Exception{
-	_log.info("Arg call: " + call + " Arg: " + _facade.getName(iteratorObject)); 
+	//_log.info("Arg call: " + call + " Arg: " + 
+	//	  _facade.getName(iteratorObject)); 
 	Class c = _facade.getClass();
 	Method[] theMethods = c.getMethods();   
 	
@@ -208,7 +241,7 @@ public class UMLInterface
      */
     public Object caller(String call)
 	throws Exception{
-	_log.info("call: " + call);
+	//_log.info("call: " + call);
 	Class c = _facade.getClass();
 	Method[] theMethods = c.getMethods();
 
