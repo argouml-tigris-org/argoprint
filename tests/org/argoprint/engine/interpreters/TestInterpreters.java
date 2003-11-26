ebacd68f71f4e55c233188bb6bbad3fb4235c823
Mikael Albertsson <abbeman@tigris.org>
2003-11-26 02:30:42 +0000

    Finished the interpreter test cases along with templates.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@97 39b8678e-d811-0410-9f66-c214488f071d

--- tests/org/argoprint/engine/interpreters/TestInterpreters.java
+++ tests/org/argoprint/engine/interpreters/TestInterpreters.java
@@ -11,17 +11,27 @@ import org.argoprint.DataSourceStub;
 
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
 public class TestInterpreters extends TestCase {
+    private class InterpreterTestException extends Exception {
+        public InterpreterTestException(String message) {
+            super(message);
+        }
+    };
+
     private InterpreterBind    _bind    = null;
     private InterpreterCall    _call    = null;
-    //private InterpreterIf      _if      = null;
+    private InterpreterIf      _if      = null;
     private InterpreterIterate _iterate = null;
     private InterpreterDefault _default = null;
 
-    private DataSourceStub _uml_int = null;
-    private Document       _doc     = null;
-    private Environment    _env     = null;
+    private DataSourceStub _uml_int            = null;
+    private Document       _correct_template   = null;
+    private Document       _incorrect_template = null;
+    private Environment    _env                = null;
+
+    private HashMap _interpreters   = null;
 
     public TestInterpreters(String s) {
         super(s);
@@ -29,94 +39,85 @@ public class TestInterpreters extends TestCase {
 
     public static Test suite() {
         // Code calling the tests.
-        /*
-        TestSuite s = new TestSuite();
-        s.addTest(new TestInterpreters("testIterate"));
-        return s;
-        */
         return new TestSuite(TestInterpreters.class);
     }
 
-    public void testIterate() throws Exception {
+    public void InterpreterTest(String name, Document doc, int num) throws Exception {
         // Get a node that should not be changed.
-        NodeList nodes  = _doc.getChildNodes().item(0).getChildNodes();
+        NodeList nodes  = doc.getChildNodes().item(0).getChildNodes();
         Node param_node = null;
-        Node comp_node  = null;
+        Node compare_node  = null;
+        name = name.toLowerCase();
 
-        int expect_pos = findNode(nodes, "expected_ap_iterate_result", 0);
-        assertTrue("expected_ap_iterate_result-node not found.", expect_pos != -1);
-        int collect_pos = findNode(nodes.item(expect_pos).getChildNodes(), "collect_iterate", 0);
-        comp_node  = nodes.item(expect_pos).getChildNodes().item(collect_pos);
+        // Get the node containing the expected result.
+        int expect_pos = findNode(nodes, "expected_ap_"+name+"_result", 0);
+        assertTrue("expected_ap_"+name+"_result-node not found.", expect_pos != -1);
+        NodeList expect_nodes = nodes.item(expect_pos).getChildNodes();
+        int collect_pos = findNode(expect_nodes, "collect_"+name, 0);
+        assertTrue("collect_"+name+"-node for expected result not found.", collect_pos != -1);
+        for (int i = 0; i < num; ++i) {
+            collect_pos = findNode(expect_nodes, "collect_"+name, collect_pos+1);
+            assertTrue("collect_"+name+"-node for expected result not found.", collect_pos != -1);
+        }
+        compare_node = expect_nodes.item(collect_pos);
 
-        collect_pos = findNode(nodes, "collect_iterate", 0);
-        assertTrue("collect_iterate-node not found.", collect_pos != -1);
-        int iterate_pos = findNode(nodes.item(collect_pos).getChildNodes(), "ap:iterate", 0);
-        assertTrue("ap:iterate-node not found.", iterate_pos != -1);
-        param_node = nodes.item(collect_pos).getChildNodes().item(iterate_pos);
+        // Get the node that collects the nodes that are to be transformed.
+        collect_pos = findNode(nodes, "collect_"+name, 0);
+        assertTrue("collect_"+name+"-node not found.", collect_pos != -1);
+        for (int i = 0; i < num; ++i) {
+            collect_pos = findNode(nodes, "collect_"+name, collect_pos+1);
+            assertTrue("collect_"+name+"-node not found.", collect_pos != -1);
+        }
+        NodeList tag_nodes = nodes.item(collect_pos).getChildNodes();
 
-        _iterate.handleTag(param_node, _env);
+        // Get the nodes that are about to be transformed.
+        int tag_pos = findNode(tag_nodes, "ap:"+name, 0);
+        assertTrue("ap:"+name+"-node not found.", tag_pos != -1);
+        Node tag_node = tag_nodes.item(tag_pos);
+        ((Interpreter)_interpreters.get(name)).handleTag(tag_node, _env);
 
         // Get the node at the same position as the call-node used to be.
         Node result_node = nodes.item(collect_pos);
 
-        ByteArrayOutputStream comp_stream   = new ByteArrayOutputStream();
-        ByteArrayOutputStream result_stream = new ByteArrayOutputStream();
+        assertTrue(name+"-interpreter does not give expected result.",
+                   nodesEqual(compare_node, result_node));
+    }
 
-        XMLSerializer ser1 = new XMLSerializer(comp_stream, null);
-        XMLSerializer ser2 = new XMLSerializer(result_stream, null);
-        ser1.serialize((Element)comp_node);
-        ser2.serialize((Element)result_node);
 
-        assertTrue("Iterate-interpreter does not give expected result.",
-                   comp_stream.toString().equals(result_stream.toString()));
+
+
+    /*
+     * Test cases.
+     *
+     */
+    public void testCorrectIterate() throws Exception {
+        InterpreterTest("iterate", _correct_template, 0);
+    }
+
+    public void testCorrectIterateWithSortvalue() throws Exception {
+        InterpreterTest("iterate", _correct_template, 1);
+    }
+
+    public void testCorrectBind() throws Exception {
+        InterpreterTest("bind", _correct_template, 0);
     }
  
-    public void testCall() throws Exception {
-        // Get a node that should not be changed.
-        NodeList nodes  = _doc.getChildNodes().item(0).getChildNodes();
-        Node param_node = null;
-        Node comp_node  = null;
-
-        // Find the position of the expected result for the call node.
-        int expect_pos = findNode(nodes, "expected_ap_call_result", 0);
-        // Find the position of the cellction-node within that node.
-        int collect_pos = findNode(nodes.item(expect_pos).getChildNodes(), "collect_call", 0);
-        comp_node  = nodes.item(expect_pos).getChildNodes().item(collect_pos);
-
-        // Find the position of the collect_call around the ap:call-node.
-        collect_pos = findNode(nodes, "collect_call", 0);
-        assertTrue(collect_pos != -1);
-        // Find the position of the ap:call-tag within that node.
-        int call_pos = findNode(nodes.item(collect_pos).getChildNodes(), "ap:call", 0);
-        assertTrue(call_pos != -1);
-        param_node = nodes.item(collect_pos).getChildNodes().item(call_pos);
-
-        _call.handleTag(param_node, _env);
-
-        // Get the node at the same position as the collect_call-node used to
-        // be.
-        Node result_node = nodes.item(collect_pos);
-
-        ByteArrayOutputStream comp_stream   = new ByteArrayOutputStream();
-        ByteArrayOutputStream result_stream = new ByteArrayOutputStream();
-
-        XMLSerializer ser1 = new XMLSerializer(comp_stream, null);
-        XMLSerializer ser2 = new XMLSerializer(result_stream, null);
-        ser1.serialize((Element)comp_node);
-        ser2.serialize((Element)result_node);
-
-        assertTrue("Call-interpreter does not give expected result.",
-                   comp_stream.toString().equals(result_stream.toString()));
+    public void testCorrectIf() throws Exception {
+        InterpreterTest("if", _correct_template, 0);
+    }
+ 
+    public void testCorrectCall() throws Exception {
+        InterpreterTest("call", _correct_template, 0);
     }
 
     public void testDefault() throws Exception {
         // Get a node that should not be changed.
-        NodeList nodes  = _doc.getChildNodes().item(0).getChildNodes();
+        NodeList nodes  = _correct_template.getChildNodes().item(0).getChildNodes();
         Node param_node = null;
         Node comp_node  = null;
 
 
-        int pos = findNode(nodes, "default", 0);
+        int pos = findNode(nodes, "default_test", 0);
 
         param_node = nodes.item(pos).cloneNode(true);
         comp_node  = nodes.item(pos).cloneNode(true);
@@ -127,17 +128,113 @@ public class TestInterpreters extends TestCase {
                    nodesEqual(param_node, comp_node));
     }
 
-    // TODO: Make sure the tests create objects correctly.
+    public void testIterateIncorrectSortvalue() throws Exception {
+        boolean exception_caught = false;
+        try {
+            InterpreterTest("iterate", _incorrect_template, 0);
+        } catch (InterpreterTestException e) {
+            throw e;
+        } catch (Exception e) {
+            exception_caught = true;
+        }
+        if (!exception_caught) {
+            fail("Exception not thrown for <iterate> with bad sortvalue.");
+        }
+    }
+
+    public void testIterateIncorrectWhat() throws Exception {
+        boolean exception_caught = false;
+        try {
+            InterpreterTest("iterate", _incorrect_template, 1);
+        } catch (InterpreterTestException e) {
+            throw e;
+        } catch (Exception e) {
+            exception_caught = true;
+        }
+        if (!exception_caught) {
+            fail("Exception not thrown for <iterate> called with nonexistant method.");
+        }
+    }
+
+    public void testIterateNoIteratorName() throws Exception {
+        boolean exception_caught = false;
+        try {
+            InterpreterTest("iterate", _incorrect_template, 2);
+        } catch (InterpreterTestException e) {
+            throw e;
+        } catch (Exception e) {
+            exception_caught = true;
+        }
+        if (!exception_caught) {
+            fail("Exception not thrown for <iterate> without iteratorname.");
+        }
+    }
+
+    public void testCallIncorrectWhat() throws Exception {
+        boolean exception_caught = false;
+        try {
+            InterpreterTest("call", _incorrect_template, 0);
+        } catch (InterpreterTestException e) {
+            throw e;
+        } catch (Exception e) {
+            exception_caught = true;
+        }
+        if (!exception_caught) {
+            fail("Exception not thrown for <call> with nonexistant method.");
+        }
+    }
+
+    public void testIfIncorrectWhat() throws Exception {
+        boolean exception_caught = false;
+        try {
+            InterpreterTest("if", _incorrect_template, 0);
+        } catch (InterpreterTestException e) {
+            throw e;
+        } catch (Exception e) {
+            exception_caught = true;
+        }
+        if (!exception_caught) {
+            fail("Exception not thrown for <if> with non-boolean method.");
+        }
+    }
+
+    public void testBindAttrWithQuote() throws Exception {
+        InterpreterTest("bind", _incorrect_template, 0);
+    }
+
+
+
+
+
+
     protected void setUp() throws IOException {
         // Prepare the nodes from the template.
         DOMParser parser = new DOMParser();
+
+        String correct_template_name = "CorrectTemplate.xml";
+        String incorrect_template_name = "IncorrectTemplate.xml";
+
+        // Parse the correct template.
         try {
-            parser.parse("org/argoprint/engine/interpreters/testtemplate.xml");
+            parser.parse("org/argoprint/engine/interpreters/" +
+                         correct_template_name);
         } catch (org.xml.sax.SAXException e) {
-            fail("Could not parse testtemplate.xml: " + e.getMessage());
+            fail("Could not parse " + correct_template_name + ": " +
+                 e.getMessage());
         }
-        _doc = parser.getDocument();
-        assertNotNull("Could not get the document.", _doc);
+        _correct_template = parser.getDocument();
+        assertNotNull("Could not get the "+correct_template_name+" from the parser.", _correct_template);
+
+        // Parse the incorrect template.
+        try {
+            parser.parse("org/argoprint/engine/interpreters/" +
+                         incorrect_template_name);
+        } catch (org.xml.sax.SAXException e) {
+            fail("Could not parse " + incorrect_template_name + ": " +
+                 e.getMessage());
+        }
+        _incorrect_template = parser.getDocument();
+        assertNotNull("Could not get " + incorrect_template_name + " from the parser.", _incorrect_template);
 
         _uml_int = new DataSourceStub();
         assertNotNull(_uml_int);
@@ -145,118 +242,83 @@ public class TestInterpreters extends TestCase {
         _env = new Environment();
         assertNotNull(_env);
 
+        _interpreters = new HashMap();
+
         _default = new InterpreterDefault(_uml_int);
         _default.setFirstHandler(_default);
-        _call    = new InterpreterCall(_uml_int);
+        _interpreters.put("default", _default);
+
+        _call = new InterpreterCall(_uml_int);
         _call.setFirstHandler(_call);
+        _call.setNextHandler(_default);
+        _interpreters.put("call", _call);
+
         _iterate = new InterpreterIterate(_uml_int);
         _iterate.setFirstHandler(_iterate);
-        _iterate.setNextHandler(_default);
-/*
-        _bind    = new InterpreterBind();
-	_bind.setFirstHandler(_bind);
-        _if      = new InterpreterIf();
-	_if.setFirstHandler(_bind);
-*/
+        _iterate.setNextHandler(_call);
+        _interpreters.put("iterate", _iterate);
+
+        _if = new InterpreterIf(_uml_int);
+        _if.setFirstHandler(_if);
+        _if.setNextHandler(_default);
+        _interpreters.put("if", _if);
+
+        _bind = new InterpreterBind(_uml_int);
+        _bind.setFirstHandler(_bind);
+        _bind.setNextHandler(_default);
+        _interpreters.put("bind", _bind);
 
 
-
-/*
         assertNotNull("Could not create InterpreterBind.",    _bind);
         assertNotNull("Could not create InterpreterIf.",      _if);
-*/
         assertNotNull("Could not create InterpreterCall.",    _call);
         assertNotNull("Could not create InterpreterIterate.", _iterate);
         assertNotNull("Could not create InterpreterDefault.", _default);
     }
 
-    private boolean nodesEqual(Node left, Node right) {
-        // Consider nodes equal if the following attributes are equal.
-        class pair {
-            public pair(String left, String right) { l = left; r = right; }
-            public String l;
-            public String r;
-        };
 
-        if (left == null || right == null) {
-            if (left != right) {
-                System.out.println("One node null");
-                return false;
-            } else {
-                return true;
-            }
-        }
+    /*
+     * Some utility functions.
+     */
 
-        if (left.getNodeType() != right.getNodeType()) {
-            System.out.println("Node types not same");
-            return false;
-        }
-        if (left.getChildNodes().getLength() != right.getChildNodes().getLength()) {
-            System.out.println("Not same amount of children");
-            System.out.println(left.getChildNodes().getLength());
-            System.out.println(right.getChildNodes().getLength());
-            return false;
-        }
+    private void outputDocument(Document doc) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        OutputFormat of = new OutputFormat(doc);
+        of.setIndent(4);
+        of.setIndenting(true);
+        of.setOmitComments(true);
+        of.setPreserveSpace(false);
+        of.setLineWidth(80);
+        (new XMLSerializer(result, of)).serialize(doc);
 
-        if (left.getAttributes() == null || right.getAttributes() == null) {
-            if (left.getAttributes() != right.getAttributes()) {
-                System.out.println("One attributes null");
-                return false;
-            }
-        } else if (!mapsEqual(left.getAttributes(), right.getAttributes())) {
-            return false;
-        }
-
-        final int length = 5;
-        pair results[] = new pair[length];
-        results[0] = new pair(left.getNodeName(), right.getNodeName());
-        results[1] = new pair(left.getNodeValue(), right.getNodeValue());
-        results[2] = new pair(left.getPrefix(), right.getPrefix());
-        results[3] = new pair(left.getNamespaceURI(), right.getNamespaceURI());
-        results[4] = new pair(left.getLocalName(), right.getLocalName());
-
-        for (int i = 0; i < length; ++i) {
-            if ((results[i].l == null && results[i].r != null) ||
-                (results[i].l != null && results[i].r == null)) {
-                return false;
-            }
-            if (results[i].l != null &&
-                results[i].r != null &&
-                !results[i].l.equals(results[i].r)) {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < right.getChildNodes().getLength(); ++i) {
-            if (!nodesEqual(right.getChildNodes().item(i),
-                            left.getChildNodes().item(i))) {
-                return false;
-            }
-        }
-
-        return true;
+        System.out.print(result.toString());
     }
 
-    private boolean mapsEqual(NamedNodeMap left, NamedNodeMap right) {
-        if (left == null || right == null) {
-            if (left == right) {
-                return true;
-            } else {
-                return false;
+    private boolean nodesEqual(Node left, Node right) throws IOException {
+        ByteArrayOutputStream lstream = new ByteArrayOutputStream();
+        ByteArrayOutputStream rstream = new ByteArrayOutputStream();
+        OutputFormat of = new OutputFormat();
+        of.setIndent(4);
+        of.setPreserveSpace(false);
+        of.setLineWidth(80);
+        (new XMLSerializer(lstream, of)).serialize((Element)left);
+        (new XMLSerializer(rstream, of)).serialize((Element)right);
+
+        String lstring = strip_ws(lstream.toString());
+        String rstring = strip_ws(rstream.toString());
+
+        return lstring.equals(rstring);
+    }
+
+    private String strip_ws(String in) {
+        StringBuffer buf = new StringBuffer(in);
+        for (int i = buf.length()-1; i >=0; --i) {
+            char c = buf.charAt(i);
+            if (c == ' ' || c == '\n' || c == '\t') {
+                buf.deleteCharAt(i);
             }
         }
-
-        if (left.getLength() != right.getLength()) {
-            return false;
-        }
-
-        for (int i = 0; i < left.getLength(); ++i) {
-            if (!nodesEqual(left.item(i), right.item(i))) {
-                return false;
-            }
-        }
-
-        return true;
+        return buf.toString();
     }
 
     private int findNode(NodeList nodes, String name, int start) {
