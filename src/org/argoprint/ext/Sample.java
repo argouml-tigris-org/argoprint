34630704c1c543c55c10ff5aec3b551d19871f3e
Bob Tarling <bobtarling@tigris.org>
2007-04-15 22:27:19 +0000

    Remove unused imports (that don't compile in java5)
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@292 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/ext/Sample.java
+++ src/org/argoprint/ext/Sample.java
@@ -2,9 +2,6 @@ package org.argoprint.ext;
 
 import java.util.Date;
 
-import org.apache.xalan.extensions.XSLProcessorContext;
-import org.apache.xalan.templates.ElemExtensionCall;
-
 public class Sample {
     public static String stamp() {
 	return (new Date()).toString();
