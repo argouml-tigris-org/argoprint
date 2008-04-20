07571ac001aadd9d8ade65c3d735b966372daa0f
Christian López Espínola <penyaskito@tigris.org>
2008-04-20 18:58:11 +0000

    Code style changes. No functional change.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@302 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argoprint/ui/ArgoPrintDialog.java
+++ src/org/argoprint/ui/ArgoPrintDialog.java
@@ -30,7 +30,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -42,148 +41,150 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.argoprint.ArgoPrint;
-
 import org.argouml.configuration.Configuration;
 import org.argouml.configuration.ConfigurationKey;
 import org.argouml.i18n.Translator;
-import org.argouml.persistence.ArgoPrintInsider;
 
 /**
  * The dialog displayed when ArgoPrint is started from the ArgoUML menu.
  */
-
-public class ArgoPrintDialog
-    extends JDialog {
+public class ArgoPrintDialog extends JDialog {
 
     private static final Dimension DEFAULT_SIZE =
-	 new Dimension(640, 480);
+        new Dimension(640, 480);
 
-    private static final String prefix = "argoprint";
+    private static final String PREFIX = "argoprint";
     private static final ConfigurationKey CONF_DEFAULT_TEMPLATE
-	= Configuration.makeKey(prefix, "default_template");
+        = Configuration.makeKey(PREFIX, "default_template");
     private static final ConfigurationKey CONF_DEFAULT_OUTPUT
-	= Configuration.makeKey(prefix, "default_output");
+        = Configuration.makeKey(PREFIX, "default_output");
 
     public ArgoPrintDialog(Frame parent, String title, boolean modal) {
-	super(parent, title, modal);
-	initComponents();
+        super(parent, title, modal);
+        initComponents();
     }
 
     private void initComponents() {
-	setSize(DEFAULT_SIZE);
-	add(ArgoPrintDialog.createSimpleDialogContent());
+        setSize(DEFAULT_SIZE);
+        add(ArgoPrintDialog.createSimpleDialogContent());
     }
 
     /*
      * Create a panel as described by the use-case in the old documentation.
      */
     private static JPanel createSimpleDialogContent() {
-	final JPanel result = new JPanel(new GridBagLayout());
-	result.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-	GridBagConstraints c = new GridBagConstraints();
+        final JPanel result = new JPanel(new GridBagLayout());
+        result.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        GridBagConstraints c = new GridBagConstraints();
 
-	final JTextField template, output;
+        final JTextField template, output;
 
-	c.fill = GridBagConstraints.BOTH;
-	c.insets = new Insets(0, 0, 3, 3);
-	c.gridx = 0;
-	c.gridy = 0;
-	c.gridwidth = 1;
-	c.weightx = 0;
-	result.add(new JLabel(Translator.localize("argoprint.label.template")), c);
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(0, 0, 3, 3);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.weightx = 0;
+        result.add(new JLabel(
+                Translator.localize("argoprint.label.template")), c);
 
-	c.gridx = 1;
-	c.weightx = 1;
-	result.add(template = new JTextField(20), c);
-	template.setText(Configuration.getString(CONF_DEFAULT_TEMPLATE));
+        c.gridx = 1;
+        c.weightx = 1;
+        result.add(template = new JTextField(20), c);
+        template.setText(Configuration.getString(CONF_DEFAULT_TEMPLATE));
 
-	c.gridx = 2;
-	c.weightx = 0;
-	result.add(new JButton(new AbstractAction(Translator.localize("argoprint.button.default")) {
-		public void actionPerformed(ActionEvent e) {
-		    Configuration.setString(CONF_DEFAULT_TEMPLATE, template.getText());
-		    Configuration.save();
-		}
-	    }), c);
-	
-	c.gridx = 0;
-	c.gridy = 1;
-	c.weightx = 0;
-	result.add(new JLabel(Translator.localize("argoprint.label.output")), c);
+        c.gridx = 2;
+        c.weightx = 0;
+        result.add(new JButton(new AbstractAction(
+                Translator.localize("argoprint.button.default")) {
+            public void actionPerformed(ActionEvent e) {
+                Configuration.setString(CONF_DEFAULT_TEMPLATE, 
+                        template.getText());
+                Configuration.save();
+            }
+        }), c);
 
-	c.gridx = 1;
-	c.weightx = 1;
-	result.add(output = new JTextField(20), c);
-	output.setText(Configuration.getString(CONF_DEFAULT_OUTPUT));
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0;
+        result.add(new JLabel(
+                Translator.localize("argoprint.label.output")), c);
 
-	c.gridx = 2;
-	c.weightx = 0;
-	result.add(new JButton(new AbstractAction(Translator.localize("argoprint.button.default")) {
-		public void actionPerformed(ActionEvent e) {
-		    Configuration.setString(CONF_DEFAULT_OUTPUT, output.getText());
-		    Configuration.save();
-		}
-	    }), c);
+        c.gridx = 1;
+        c.weightx = 1;
+        result.add(output = new JTextField(20), c);
+        output.setText(Configuration.getString(CONF_DEFAULT_OUTPUT));
 
-	c.gridx = 2;
-	c.gridy = 2;
-	c.weightx = 0;
-	result.add(new JButton(new AbstractAction(Translator.localize("argoprint.button.execute")) {
-		public void actionPerformed(ActionEvent e) {
-		    InputStream streamTemplate = null;
-		    OutputStream streamOutput = null;
+        c.gridx = 2;
+        c.weightx = 0;
+        result.add(new JButton(new AbstractAction(
+                Translator.localize("argoprint.button.default")) {
+            public void actionPerformed(ActionEvent e) {
+                Configuration.setString(CONF_DEFAULT_OUTPUT, output.getText());
+                Configuration.save();
+            }
+        }), c);
 
-		    try {
-			streamTemplate = new FileInputStream(template.getText());
-		    } catch (FileNotFoundException ex) {
-			JOptionPane.showMessageDialog(result,
-						      Translator
-						      .localize("argoprint.message.wrongTemplate"));
-			return;
-		    }
-		    
-		    try {
-			streamOutput = new FileOutputStream(output.getText());
-		    } catch (FileNotFoundException ex) {
-			JOptionPane.showMessageDialog(result,
-						      Translator
-						      .localize("argoprint.message.wrongOutput"));
-			return;
-		    }
+        c.gridx = 2;
+        c.gridy = 2;
+        c.weightx = 0;
+        result.add(new JButton(new AbstractAction(
+                Translator.localize("argoprint.button.execute")) {
+            public void actionPerformed(ActionEvent e) {
+                InputStream streamTemplate = null;
+                OutputStream streamOutput = null;
 
-		    // something smart to replace this?
-		    final InputStream paramTemplate = streamTemplate;
-		    final OutputStream paramOutput = streamOutput;
+                try {
+                    streamTemplate = new FileInputStream(template.getText());
+                } catch (FileNotFoundException ex) {
+                    JOptionPane.showMessageDialog(result,
+                            Translator
+                            .localize("argoprint.message.wrongTemplate"));
+                    return;
+                }
 
-		    Thread t = new Thread() {
-			    public void run() {
-				try {
-				    setEnabled(false);
-				    ArgoPrint.generate(paramTemplate,
-						       paramOutput);
-				    JOptionPane.showMessageDialog(result,
-								  Translator
-								  .localize("argoprint.message.transformationDone"));
-				} catch (javax.xml.transform.TransformerException ex) {
-				    JOptionPane.showMessageDialog(result,
-								  Translator
-								  .localize("argoprint.message.transformationError"));
-				    return;
-				} finally {
-				    setEnabled(true);
-				}
-				
-			    }
-			};
-		    t.start();
-		}
-	    }), c);
+                try {
+                    streamOutput = new FileOutputStream(output.getText());
+                } catch (FileNotFoundException ex) {
+                    JOptionPane.showMessageDialog(result,
+                            Translator
+                            .localize("argoprint.message.wrongOutput"));
+                    return;
+                }
 
-	return result;
+                // something smart to replace this?
+                final InputStream paramTemplate = streamTemplate;
+                final OutputStream paramOutput = streamOutput;
+
+                Thread t = new Thread() {
+                    public void run() {
+                        try {
+                            setEnabled(false);
+                            ArgoPrint.generate(paramTemplate,
+                                    paramOutput);
+                            JOptionPane.showMessageDialog(result,
+                                    Translator
+                                    .localize("argoprint.message.transformationDone"));
+                        } catch (javax.xml.transform.TransformerException ex) {
+                            JOptionPane.showMessageDialog(result,
+                                    Translator
+                                    .localize("argoprint.message.transformationError"));
+                            return;
+                        } finally {
+                            setEnabled(true);
+                        }
+
+                    }
+                };
+                t.start();
+            }
+        }), c);
+
+        return result;
     }
 }
