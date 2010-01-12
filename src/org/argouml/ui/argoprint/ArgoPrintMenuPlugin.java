b922d8bf4b0c051a9b070c32fbe7ecb61d3bb0f8
Linus Tolke <linus@tigris.org>
2010-01-12 20:12:30 +0000

    Added EPL License header.
    git-svn-id: http://argoprint.tigris.org/svn/argoprint/trunk@344 39b8678e-d811-0410-9f66-c214488f071d

--- src/org/argouml/ui/argoprint/ArgoPrintMenuPlugin.java
+++ src/org/argouml/ui/argoprint/ArgoPrintMenuPlugin.java
@@ -1,4 +1,18 @@
-// $Id$
+/* $Id$
+ *****************************************************************************
+ * Copyright (c) 2009 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    phidias
+ *****************************************************************************
+ *
+ * Some portions of this file was previously release using the BSD License:
+ */
+
 // Copyright (c) 2003-2005, Linus Tolke, Mikael Albertsson, Mattias Danielsson,
 // Per Engstrom, Fredrik Grundahl, Martin Gyllensten, Anna Kent, Anders Olsson,
 // Mattias Sidebeck.
